df772f6f489e0ed8dcfdd13190720bddbec86984
Ol'ga Krit <odhazzar@ox-i5.msk.21-school.ru>
2020-10-12 19:40:30 +0300

    add main

--- main.c
+++ main.c
@@ -14,9 +14,11 @@
  */
 int lsh_cd(char **args, t_ms *ms);
 int lsh_pwd(char **args, t_ms *ms);
-int lsh_help(char **args, t_ms *ms);
+//int lsh_help(char **args, t_ms *ms);
 int lsh_exit(char **args, t_ms *ms);
 int lsh_echo(char **args, t_ms *ms);
+int lsh_env(char **args, t_ms *ms);
+int lsh_export(char **args, t_ms *ms);
 
 /*
 	Список встроенных команд, за которыми следуют соответствующие функции
@@ -26,16 +28,19 @@ char *builtin_str[] = {
 	"pwd",
 	"exit",
 	"echo",
+	"env",
 	"export",
-	"unset",
-	"env"};
+//	"unset"
+	};
 
 int (*builtin_func[])(char **, t_ms *) = {
 	&lsh_cd,
 	&lsh_pwd,
 //	&lsh_help,
 	&lsh_exit,
-	&lsh_echo};
+	&lsh_echo,
+	&lsh_env,
+	&lsh_export};
 
 /*int ft_strlen(char *s) 
 {
@@ -64,9 +69,9 @@ char	*ft_strdup(char *s)
 	}
 	out[i] = '\0';
 	return (out);
-}
+} */
 
-int ft_strcmp(char *s1, char *s2)
+int ft_strcmp1(char *s1, char *s2)
 {
 	int i;
 
@@ -83,9 +88,26 @@ int ft_strcmp(char *s1, char *s2)
 	if (s1[i] != s2[i])
 		return (0);
 	return (1);
-}*/
+}
 
-static char	**free_arr(char **arr)
+int ft_strcmp2(char *s1, char *s2, int count)
+{
+	int i;
+
+	i = 0;
+	if (s1 && s2)
+	{
+		while (s1[i] && i < count)
+		{
+			if (s1[i] != s2[i])
+				return (0);
+			i++;
+		}
+	}
+	return (1);
+}
+
+/*static char	**free_arr(char **arr)
 {
 	unsigned int i;
 
@@ -97,7 +119,7 @@ static char	**free_arr(char **arr)
 	}
 	free(arr);
 	return (NULL);
-}
+}*/
 
 int lsh_num_builtins()
 {
@@ -107,19 +129,100 @@ int lsh_num_builtins()
 /*
 	Реализации встроенных функций
 */
+char *find_in_env(t_ms *ms, char *s)
+{
+	t_env *tmp;
+
+	tmp = ms->env;
+	while (tmp->next)
+	{
+		if (ft_strcmp2(tmp->name, s, ft_strlen(s)))
+			return(tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+void replace_pwd(t_ms *ms)
+{
+	t_env *tmp;
+	t_env *pwd;
+	t_env *old_pwd;
+
+	tmp = ms->env;
+	while (tmp->next)
+	{
+		if (ft_strcmp2(tmp->name, "PWD", 3))
+			pwd = tmp;
+		if (ft_strcmp2(tmp->name, "OLDPWD", 6))
+			old_pwd = tmp;
+		tmp = tmp->next;
+	}
+	free(old_pwd->value);
+	old_pwd->value = pwd->value;
+	pwd->value = NULL;
+	pwd->value = getcwd(pwd->value, 0);
+}
+
+//void add_in_env(t_ms **ms, char *s)
+void add_in_env(t_ms *ms, char *s)
+{
+	char **test;
+	t_env *tmp;
+	t_env *new;
+	//t_ms *re = *(ms);
+	//t_env *new;
+
+	test = ft_split(s, '=');
+	//printf("s = %s test[0] = %s  test[1] = %s \n", s, test[0], test[1]);
+	//tmp = (*ms)->env;
+	tmp = ms->env;
+	//while (tmp->next && tmp->next->name)
+	while (tmp->next->next)
+		tmp = tmp->next;
+	printf("data = %s  value = %s \n", tmp->name, tmp->value);
+	//printf("data = %s  value = %s \n", tmp->next->name, tmp->next->value);
+	//new = malloc(sizeof(t_env));
+	//tmp->next = new;
+	tmp->next = malloc(sizeof(t_env));
+	tmp = tmp->next;
+	tmp->name = test[0];
+	tmp->value = test[1];
+	tmp->next = NULL;
+	//printf("data = %s value = %s next_data = %s  next_value = %s \n", tmp->name, tmp->value, tmp->next->name, tmp->next->value);
+
+	/*tmp = malloc(sizeof(t_env));
+	tmp->name = test[0];
+	tmp->value = test[1];
+	tmp->next = NULL;*/
+	printf("data = %s value = %s \n", tmp->name, tmp->value);
+}
+
+void delete_from_env(t_ms *ms, char *s)
+{
+
+}
 
 int lsh_pwd(char **args, t_ms *ms)
 {
-	write(1, ms->path, ft_strlen(ms->path));
+	char *s;
+	s = find_in_env(ms, "PWD");
+	//printf("s = %s  \n", s);
+	//write(1, ms->path, ft_strlen(ms->path));
+	write(1, s, ft_strlen(s));
 	write(1, "\n", 1);
 	return (1);
 }
 
 int lsh_cd(char **args, t_ms *ms)
 {
+	char *s;
 	if (args[1] == NULL)
 	{
-		if (chdir(ms->home) != 0)
+		s = find_in_env(ms, "HOME");
+		//printf("s = %s  \n", s);
+		//if (chdir(ms->home) != 0)
+		if (chdir(s) != 0)
 			perror("cd");
 		//fprintf(stderr, "lsh: ожидается аргумент для \"cd\"\n");
 	}
@@ -128,9 +231,10 @@ int lsh_cd(char **args, t_ms *ms)
 		if (chdir(args[1]) != 0)
 			perror("cd");
 	}
-	free(ms->path);
+	replace_pwd(ms);
+	/*free(ms->path);
 	ms->path = NULL;
-	ms->path = getcwd(ms->path, 0);
+	ms->path = getcwd(ms->path, 0);*/
 	//printf("path = %s \n", ms->path);
 
 	return 1;
@@ -139,7 +243,7 @@ int lsh_cd(char **args, t_ms *ms)
 int lsh_echo(char **args, t_ms *ms)
 {
 	//printf ("args = |%s| \n", args[1]);
-	if (ft_strcmp(args[1], "-n"))
+	if (ft_strcmp1(args[1], "-n"))
 		write(1, args[2], ft_strlen(args[2]));
 	else
 	{
@@ -152,7 +256,40 @@ int lsh_echo(char **args, t_ms *ms)
 int lsh_exit(char **args, t_ms *ms)
 {
 	return 0;
-} 
+}
+
+int lsh_env(char **args, t_ms *ms)
+{
+	t_env *tmp;
+
+	tmp = ms->env;
+	while (tmp->next)
+	{
+		write(1, tmp->name, ft_strlen(tmp->name));
+		write(1, "=", 1);
+		write(1, tmp->value, ft_strlen(tmp->value));
+		write(1, "\n", 1);
+		//if (tmp->next)
+		tmp = tmp->next;
+	}
+	if (tmp->name)
+	{
+		write(1, tmp->name, ft_strlen(tmp->name));
+		write(1, "=", 1);
+		write(1, tmp->value, ft_strlen(tmp->value));
+		write(1, "\n", 1);
+	}
+	return (1);
+}
+
+int lsh_export(char **args, t_ms *ms)
+{
+	t_env *tmp;
+
+	if (args[1])
+		add_in_env(ms, args[1]);
+	return(1);
+}
 
 char *unite_str(char *path)
 {
@@ -177,7 +314,7 @@ char *unite_str(char *path)
 	return (res);
 }
 
-int lsh_launch(char **args, char **envp, t_ms *ms)
+int lsh_launch(char **args, t_ms *ms)
 {
 	pid_t pid, wpid;
 	int status;
@@ -252,7 +389,7 @@ char *lsh_read_line(void)
 	return line;
 }
 
-int lsh_execute(char **args, char **envp, t_ms *ms)
+int lsh_execute(char **args, t_ms *ms)
 {
 	int i = 0;
 
@@ -264,10 +401,10 @@ int lsh_execute(char **args, char **envp, t_ms *ms)
 			return (*builtin_func[i])(args, ms);
 		i++;
 	}
-	return lsh_launch(args, envp, ms);
+	return lsh_launch(args, ms);
 }
 
-void lsh_loop(char **envp, t_ms *ms)
+void lsh_loop(t_ms *ms)
 {
 	char *line;
 	char **args;
@@ -279,7 +416,7 @@ void lsh_loop(char **envp, t_ms *ms)
 		line = lsh_read_line();
 		args = lsh_split_line(line);
 		//printf("line = %s\n", line);
-		status = lsh_execute(args, envp, ms);
+		status = lsh_execute(args, ms);
 		free(line);
 		free(args);
 	}
@@ -319,12 +456,14 @@ int main(int argc, char **argv, char **envp)
 		//i++;
 	}
 	env->next = NULL;
-	while (first)
+	env = first;
+	ms.env = env;
+	/*while (first->next)
 	{
 		printf("name = %s \n value = %s \n", first->name, first->value);
 		first = first->next;
-	}
-	lsh_loop(envp, &ms);
+	}*/
+	lsh_loop(&ms);
 
 	// Выключение / очистка памяти.
 
