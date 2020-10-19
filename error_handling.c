23a1c75cd7101ac5f1ebda9abd2bb402f15dff91
Ольга <tekuschaya@MacBook.local>
2020-10-19 23:29:26 +0300

    change error

--- error_handling.c
+++ error_handling.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-int throw_error(char *errtype, t_ms *ms)
+/*int throw_error(char *errtype, t_ms *ms)
 {
 	ft_putstr_fd("minishell: ", 2);
 	if (!ft_strcmp(errtype, MEMALLOC))
@@ -43,4 +43,31 @@ int throw_error(char *errtype, t_ms *ms)
 		exit(126);
 	}
 	return (1);
+}*/
+
+int throw_error(char *errtype, t_ms *ms)
+{
+	ft_putstr_fd("minishell: ", 2);
+	if (!ft_strcmp(errtype, MEMALLOC))
+	{
+		ft_putendl_fd(errtype, 2);
+		exit(EXIT_FAILURE);
+	}	
+	else if (!ft_strcmp(errtype, PARSEERR))
+	{
+		ft_putendl_fd(PARSEERR, 2);
+		tcmd_free(ms);
+		ms->ret = 258;
+	}
+	else
+	{
+		ft_putstr_fd(ms->cmd->name, 2);
+		ft_putendl_fd(errtype, 2);
+		if (!ft_strcmp(errtype, CMDNFERR) || !ft_strcmp(errtype, CDERR))
+			exit(127);
+		else if (!ft_strcmp(errtype, ISADIRERR) || !ft_strcmp(errtype, PERMISSIONERR))
+			exit(126);
+	}
+	return (0);
 }
+
