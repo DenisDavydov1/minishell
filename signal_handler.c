8993f7c17df29ab2d63bf552328b61bb22e40a5a
Ol'ga Krit <odhazzar@ox-l3.msk.21-school.ru>
2020-10-20 13:37:51 +0300

    little change

---
+++ signal_handler.c
@@ -0,0 +1,17 @@
+#include "minishell.h"
+
+void	sigint_handler(int sn)
+{
+	if (write(1, "\b\b  \n", 5) < 0)
+		return ;
+	//display_prompt_msg();
+    write(1, "minishell-1.0$ ", 15);
+	signal(SIGINT, sigint_handler);
+}
+
+void	sigquit_handler(int sn)
+{
+	if (write(1, "\b\b  \b\b", 6) < 0)
+		return ;
+	return ;
+}
